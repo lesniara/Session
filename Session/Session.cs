405e570db8b33d1b6c21d0b32d627782114a2f70
Alex Lesniara <alesniara@iqmh.org>
2021-05-16 18:01:04 -0400

    Add project files.

---
+++ Session/Session.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+
+
+namespace Lesniarasoft.Client
+{
+    /// <summary>
+    /// ClientSession class is public, static ,and reachable from anywhere in code.
+    /// </summary>
+    public static class Session
+    {
+        private static Dictionary<string, object> _Bag = new Dictionary<string, object>();
+        private static object _lock = new object();
+
+        /// <summary>
+        /// Internal backing Dictionary variable
+        /// </summary>
+        public static Dictionary<string, object> Bag {
+            get{
+                lock (_lock)
+                {
+                    return _Bag;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Locate or initialize and set the key/value pair
+        /// </summary>
+        /// <typeparam name="T">Generic Type</typeparam>
+        /// <param name="key">Session value lookup unique key</param>
+        /// <param name="value">Session variable value</param>
+        public static void TrySet<T>(string key, T value)
+        {
+            lock (_lock)
+            {
+                if (!_Bag.ContainsKey(key))
+                {
+                    _Bag.Add(key, null);
+                }
+
+                _Bag[key] = value;
+            }
+        }
+
+        /// <summary>
+        /// Retrieve an existing 
+        /// </summary>
+        /// <typeparam name="T">Generic Type</typeparam>
+        /// <param name="key">Key to retrieve the associated session value</param>
+        /// <param name="value">Session value</param>
+        public static void TryGet<T>(string key, out T value)
+        {
+            lock (_lock)
+            {
+                if (_Bag.ContainsKey(key))
+                {
+                    value = (T)_Bag[key];
+                }
+                else
+                {
+                    value = default;
+                }
+            }
+        }
+        /// <summary>
+        /// Determines whether the key exists
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool ContainsKey(string key)
+        {
+            lock (_lock)
+            {
+                return _Bag.ContainsKey(key);
+            }
+        }
+    }
+}
---
+++ SessionDemo/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Lesniarasoft.Client;
+
+namespace SessionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Session Demo!");
+            StringTest();
+            Console.WriteLine();
+
+            IntegerTest();
+            Console.WriteLine();
+
+            ListTest();
+            Console.WriteLine();
+
+            ChangeFirstStringTest();
+            Console.WriteLine();
+
+            Console.WriteLine("All Done!");
+        }
+
+        static void StringTest()
+        {
+            Console.WriteLine("Enter Session Key:");
+            var sessionKey = Console.ReadLine();
+
+            Console.WriteLine("Enter Session String Value:");
+            var sessionValue = Console.ReadLine();
+
+            // write
+            Session.TrySet<string>(sessionKey, sessionValue);
+
+            // read
+            Session.TryGet<string>(sessionKey, out sessionValue);
+
+            Console.WriteLine($"Session String Key: {sessionKey}, Value:{sessionValue}");
+        }
+
+        static void IntegerTest()
+        {
+            Console.WriteLine("Enter Session Key:");
+            var sessionKey = Console.ReadLine();
+
+            Console.WriteLine("Enter Session Integer Value:");
+            // sanitize command line input
+            int sessionValue = 0;
+            Int32.TryParse(Console.ReadLine(), out sessionValue);
+
+            // write 
+            Session.TrySet<int>(sessionKey, sessionValue);
+
+            // read
+            Session.TryGet<int>(sessionKey, out sessionValue);
+
+            Console.WriteLine($"Session Integer Key: {sessionKey}, Value:{sessionValue}");
+        }
+
+        static void ListTest()
+        {
+            Console.WriteLine("Enter Session Key:");
+            var sessionKey = Console.ReadLine();
+
+            Console.WriteLine("Enter comma separated values:");
+            var sessionValue = Console.ReadLine().Split(",").ToList<string>();
+
+            // write
+            Session.TrySet<List<string>>(sessionKey, sessionValue);
+
+            // read
+            Session.TryGet<List<string>>(sessionKey, out sessionValue);
+
+            Console.WriteLine($"Session List Key: {sessionKey}, Values:");
+
+            foreach (string item in sessionValue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void ChangeFirstStringTest()
+        {
+            var firstStringKey = Session.Bag.Keys.First();
+            string originalValue;
+
+            Session.TryGet(firstStringKey, out originalValue);
+
+            Console.WriteLine("Changing the original string value:");
+            Console.WriteLine($"Key: {firstStringKey}, Value: {originalValue}");
+            Console.WriteLine("Enter New Session String Value:");
+            var sessionValue = Console.ReadLine();
+
+            // write
+            Session.TrySet<string>(firstStringKey, sessionValue);
+
+            // read
+            Session.TryGet<string>(firstStringKey, out sessionValue);
+
+            Console.WriteLine($"Session String Key: {firstStringKey}, Value:{sessionValue}");
+        }
+
+    }
+}
